8d9a705629134f3d411802a76f32d12d43a90f00
Apurva <apurwa.rokade@gmail.com>
2024-03-20 14:29:54 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+   <>
+   {/* <div class="centerText"><span>Checkout associated </span><a href="http://www.hartzis.me/react-image-upload/" target="_blank">blog post</a></div> */}
+   </>
+  )
+}
+
+export default Footer
